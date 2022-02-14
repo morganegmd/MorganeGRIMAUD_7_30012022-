2348a513b4b0b5f6a4cdf0638ba5f29e5a7d6d69
morganegmd33 <morgane.grimaud112@gmail.com>
2022-02-14 18:35:28 +0100

    config ok

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+// Cors (need to create a config file for better lisibility)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+// Routes files
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const commentRoutes = require("./routes/comment");
+
+// Middlewares always executed
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+module.exports = app;
---
+++ backend/routes/post
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const postCtrl = require("../controllers/post.controller");
+const auth = require("../middlewares/auth.middleware");
+const upload = require("../middlewares/multer-config");
+
+// Post CRUD
+router.get("/", auth, postCtrl.getAllPosts);
+router.get("/:id", auth, postCtrl.getOnePost);
+router.post("/", auth, upload.single("post_image"), postCtrl.createPost);
+router.delete("/:id", auth, postCtrl.deleteOnePost);
+router.put("/:id", auth, postCtrl.updatePost);
+
+// Images
+router.get("/image/:id", auth, postCtrl.getOneImage);
+
+// Like / Unlike
+router.patch("/:id/likeunlike", auth, postCtrl.likeUnlikePost);
+router.post("/:id/postLikedByUser", auth, postCtrl.postLikedByUser);
+router.post("/:id/likeunlike", auth, postCtrl.countLikes);
+
+module.exports = router;
